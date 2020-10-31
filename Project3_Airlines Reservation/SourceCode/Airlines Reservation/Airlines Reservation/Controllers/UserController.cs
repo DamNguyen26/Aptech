6851897af6514e877adad5291363244de221a36a
Nguyên <71805130+DamNguyen26@users.noreply.github.com>
2020-10-31 12:39:58 +0700

    update

---
+++ Project3_Airlines Reservation/SourceCode/Airlines Reservation/Airlines Reservation/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Airlines_Reservation.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Guide()
+        {
+            return View();
+        }
+
+        public ActionResult Conditions()
+        {
+            ViewBag.Message = "flight booking conditions.";
+
+            return View();
+        }
+    }
+}
